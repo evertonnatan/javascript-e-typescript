b87c84ea15ec4e813bc3d31e5269cbd8a963e9e4
evertonnatan <evertonnatandl@outlook.com>
2021-06-06 17:33:43 -0300

    console.log

---
+++ 02 - javascript básico/consolelog.js
@@ -0,0 +1,6 @@
+console.log("Hello, 'World'");
+console.log('Hello, "World"');
+console.log(`Hello, World`);
+
+// EXERCÍCIO:
+console.log('Meu nome é "Everton". Estou aprendendo JavaScript às ' + 10 + ` da manhã`);
