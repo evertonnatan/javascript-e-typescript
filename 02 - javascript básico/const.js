06613df839b8485bb8fc25648718185eba4bcccb
evertonnatan <evertonnatandl@outlook.com>
2021-06-07 09:33:20 -0300

    entendendo o basico de let e const

---
+++ 02 - javascript básico/const.js
@@ -0,0 +1,11 @@
+// String = Text | Number = Número
+
+const primeiroNumero = "5";
+const segundoNumero = 10;
+
+const resultado = primeiroNumero * segundoNumero;
+
+const resultadoDuplicado = resultado * 2;
+
+
+console.log(typeof(primeiroNumero + segundoNumero));
---
+++ 02 - javascript básico/let.js
@@ -0,0 +1,9 @@
+// Não se pode criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não de pode começar o nome de uma variável com um número
+// Nomes de variáveis não podem conter espaços ou traços
+// Utiliza-se camelCase
+// Variáveis são case-sensitive
+// Não se pode redeclarar variáveis com "let"
+
+
