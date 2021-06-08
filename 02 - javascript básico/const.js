cbcae2d1f2289efe3d87b54dac6290ea33138e8a
evertonnatan <evertonnatandl@outlook.com>
2021-06-08 08:41:05 -0300

    Resolvendo o exercicio da aula 10

--- 02 - javascript básico/const.js
+++ 02 - javascript básico/const.js
@@ -9,3 +9,21 @@ const resultadoDuplicado = resultado * 2;
 
 
 console.log(typeof(primeiroNumero + segundoNumero));
+
+//EXERCÍCIO AULA 10:
+
+const nome = 'Everton';
+const sobrenome = 'Natan';
+const idade = 30;
+const peso = 69;
+const altura = 1.62;
+let imc; // fórmula: peso/(altura * altura);
+let anoNascimento;
+let today = new Date();
+let year = today.getUTCFullYear();
+
+
+imc = peso / (altura * altura);
+anoNascimento = year - idade
+
+console.log(nome, sobrenome, "tem", idade, "anos, pesa", peso, "KG, tem", altura, "de altura e seu IMC é de", imc, nome, sobrenome, "nasceu em", anoNascimento )
