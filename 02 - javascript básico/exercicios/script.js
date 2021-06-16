f76ea48b7e684b30c63f9e10a656fd55c92873a5
evertonnatan <evertonnatandl@outlook.com>
2021-06-16 09:50:26 -0300

    Resolvendo o exercicio de variaveis

--- 02 - javascript básico/exercicios/script.js
+++ 02 - javascript básico/exercicios/script.js
@@ -1,7 +1,24 @@
-let num1 = prompt('Digite um número');
-let num2 = prompt('Digite outro número');
+// let num1 = prompt('Digite um número');
+// let num2 = prompt('Digite outro número');
 
-num1 = Number(num1);
-num2 = Number(num2);
+// num1 = Number(num1);
+// num2 = Number(num2);
 
-alert(`O resultado da sua conta foi: ${num1 + num2}`)
+// alert(`O resultado da sua conta foi: ${num1 + num2}`)
+
+// EXERCÍCIO DE VARIÁVEIS
+
+let varA = 'A'; //B
+let varB = 'B'; //C
+let varC = 'C'; //A
+
+const varATemporaria = varA;
+
+varA = varB;
+varB = varC;
+varC = varATemporaria;
+
+// Resolução alternativa:
+// [varA, varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC)
