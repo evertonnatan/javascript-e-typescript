c48714581a3e7bd4ae41e856426fe0701243b809
evertonnatan <evertonnatandl@outlook.com>
2021-06-22 21:48:00 -0300

    Exercicios de Number

---
+++ 02 - javascript básico/exercicios/math.js
@@ -0,0 +1,10 @@
+let num1 = 9.54321;
+
+//let num2 = Math.floor(num1); // 9
+//let num2 = Math.ceil(num1); // 10
+//let num2 = Math.round(num1); // arredonda para o mais próximo
+
+//console.log(num2);
+
+// GERANDO O Infinity:
+console.log( 100 / 0);
