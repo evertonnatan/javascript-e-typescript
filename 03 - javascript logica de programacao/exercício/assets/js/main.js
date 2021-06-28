89451b4a5f7338981063453a9e42f104726374db
evertonnatan <evertonnatandl@outlook.com>
2021-06-28 09:05:01 -0300

    Iniciando a resoluçao do exercicio de 'if else'

---
+++ 03 - javascript logica de programacao/exercício/assets/js/main.js
@@ -0,0 +1,12 @@
+/*
+O IMC é a relação entre peso e altura e o cálculo é feito de acordo com a fórmula: IMC = peso/ (altura x altura), 
+devendo o peso estar em kg e a altura em metro, e o resultado é dado em kg/m2. 
+
+Depois de obter o resultado, é verificado em que faixa o resultado se encontra, podendo indicar:
+
+Magreza, quando o resultado é menor que 18,5 kg/m2;
+Normal, quando o resultado está entre 18,5 e 24,9 kg/m2;
+Sobrepeso, quando o resultado está entre 24,9 e 30 kg/m2;
+Obesidade, quando o resultado é maior que 30 kg/m2.
+
+*/
