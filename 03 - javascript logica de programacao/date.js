71e5c2625582e16e19e63b11175351fa535aa0e2
evertonnatan <evertonnatandl@outlook.com>
2021-07-01 20:49:45 -0300

    Criando uma função que retorna a data, hora, minutos e segundos, de acordo com o padrão brasileiro

---
+++ 03 - javascript logica de programacao/date.js
@@ -0,0 +1,24 @@
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+// const data = new Date(0);
+
+// console.log(data)
+
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+function formataData(data) {
+    const dia = zeroAEsquerda(data.getDate());
+    const mes = zeroAEsquerda(data.getMonth() + 1);
+    const ano = zeroAEsquerda(data.getFullYear());
+    const hora = zeroAEsquerda(data.getHours());
+    const minutos = zeroAEsquerda(data.getMinutes());
+    const segundos = zeroAEsquerda(data.getSeconds());
+
+    return `${dia}/${mes}/${ano}  ${hora}:${minutos}:${segundos}`;
+};
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
