a1b6f5d4be9a2f9d982dc3c20c39ba7a94825adf
evertonnatan <evertonnatandl@outlook.com>
2021-07-01 19:56:19 -0300

    Aprendendo sobre o operador ternário

---
+++ 03 - javascript logica de programacao/ternario.js
@@ -0,0 +1,16 @@
+// ? : 
+
+const pontuacaoUsuario = 1900;
+
+if (pontuacaoUsuario >= 1000) {
+    console.log('Usuário VIP');
+} else {
+    console.log('Usuário normal')
+};
+
+// RESCREVENDO O CÓDIGO ACIMA USANDO O OPERADOR TERNÁRIO:
+
+const imdb = 8;
+
+const qualidadeDoFilme = imdb >= 7 ? "Filme bom" : "Filme ruim";
+console.log(qualidadeDoFilme);
