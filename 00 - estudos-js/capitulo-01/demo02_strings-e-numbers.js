42a3095565d2c537bd9caa4e076053cdcc48d671
evertonnatan <evertonnatandl@outlook.com>
2021-06-30 21:47:24 -0300

    Testando o Rigolith Linux virtualizado

--- 00 - estudos-js/capitulo-01/demo02_strings-e-numbers.js
+++ 00 - estudos-js/capitulo-01/demo02_strings-e-numbers.js
@@ -9,3 +9,10 @@ console.log(typeof(meuSalario), meuSalario)
 
 let issoEUmaSring = "中文 español English हिन्दी العربية português বাংলা русский 日本語 ਪੰਜਾਬੀ 한국어"
 console.log(issoEUmaSring)
+
+// Há duas maneiras de acessar um caractere individual em uma string. A primeira é o método "charAt":
+
+let stringTeste = "cat"
+console.log(stringTeste.charAt(0))
+console.log(stringTeste.charAt(1))
+console.log(stringTeste.charAt(2))
