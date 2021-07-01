bb790e50d738de2599c530d226587c96ce59812a
evertonnatan <evertonnatandl@outlook.com>
2021-06-30 21:29:11 -0300

    Testando a Máquina Virtual Rigolith Linux

--- 00 - estudos-js/capitulo-01/demo02_strings-e-numbers.js
+++ 00 - estudos-js/capitulo-01/demo02_strings-e-numbers.js
@@ -6,3 +6,6 @@ let meuSalarioCorrigido = Number(meuSalario);
 console.log(salarioDoAmigo + meuSalarioCorrigido)
 console.log(typeof(salarioDoAmigo), salarioDoAmigo)
 console.log(typeof(meuSalario), meuSalario)
+
+let issoEUmaSring = "中文 español English हिन्दी العربية português বাংলা русский 日本語 ਪੰਜਾਬੀ 한국어"
+console.log(issoEUmaSring)
