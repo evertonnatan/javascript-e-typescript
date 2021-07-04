bb7b2a482a5864aa3c4a015af13d46ee8c5341d6
evertonnatan <evertonnatandl@outlook.com>
2021-07-04 16:49:32 -0300

    Mais diferenças entre 'let', 'var' e 'const'

---
+++ 03 - javascript logica de programacao/let_var_const.js
@@ -0,0 +1,14 @@
+// DIFERENÇAS ENTRE var, let e const
+
+// LET TEM ESCOPO DE BLOCO, VEJA:
+
+{
+    let nome = 'Everton';
+    console.log(nome);
+};
+
+{ 
+    let nome = 'Everton';
+    console.log(nome);
+
+};
