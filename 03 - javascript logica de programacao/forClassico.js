5ba3a6d810cf75f5211ead599081155d9b344bc8
evertonnatan <evertonnatandl@outlook.com>
2021-07-06 06:57:22 -0300

    Otima aula sobre laço for

---
+++ 03 - javascript logica de programacao/forClassico.js
@@ -0,0 +1,15 @@
+// "i" vem de "index"
+for (let i = 0; i <= 5; i++) {
+    console.log(`Linh ${i}`);
+}
+
+// "for" negativo
+for (let i = 10; i >= 5; i--) {
+    console.log(`Linh ${i}`);
+}
+
+const frutas = ["Maçã", "Pêra", "Uva", "Banana", "Abacaxi", "Mamão", "Laranja"];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`, frutas[i]);
+}
