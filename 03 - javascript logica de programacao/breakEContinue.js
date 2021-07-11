ba723941319f9fb95fc3986dd2f9a4736cde28bf
evertonnatan <evertonnatandl@outlook.com>
2021-07-10 22:09:01 -0300

    Resolvendo o exercício de lógica

--- 03 - javascript logica de programacao/breakEContinue.js
+++ 03 - javascript logica de programacao/breakEContinue.js
@@ -11,3 +11,15 @@ for (let numero of numeros) {
 
     console.log(numero)
 }
+
+// EXERCÍCIO: escreva uma função que recebe 2 números e retorne o maior deles
+
+function recebeNumero (num1, num2) {
+    if (num1 > num2) {
+        return num1
+    } else {
+        return num2
+    }
+}
+
+console.log(recebeNumero(9, 8));
