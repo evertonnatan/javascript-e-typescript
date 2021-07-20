2b1681a2ef38cd6972674e7f7097749e01e03f73
evertonnatan <evertonnatandl@outlook.com>
2021-07-20 07:11:09 -0300

    Resolvendo o exercício 'Criação de um timer com JS'

--- 03 - javascript logica de programacao/exercicio_05/assets/js/main.js
+++ 03 - javascript logica de programacao/exercicio_05/assets/js/main.js
@@ -1,12 +1,40 @@
-const paragrafos = document.querySelector(".paragrafos");
-const ps = paragrafos.querySelectorAll('p');
-
-const estilosBody = getComputedStyle(document.body);
-const backgroundColorBody = estilosBody.backgroundColor;
-const corTexto = estilosBody.color;
-console.log(backgroundColorBody);
-
-for (let p of ps) {
-    p.style.backgroundColor = backgroundColorBody;
-    p.style.color = "#ffffff";
+function getHourFromSeconds(segungos) {
+    const data = new Date(segungos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    });
 }
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
+
+function iniciaRelogio() {
+    timer = setInterval(function() {
+        segundos++;
+        relogio.innerHTML = getHourFromSeconds(segundos);
+    }, 1000);
+}
+
+iniciar.addEventListener('click', function(event) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciaRelogio();
+});
+
+pausar.addEventListener('click', function(event) {
+    clearInterval(timer);
+    relogio.classList.add('pausado');
+
+});
+
+zerar.addEventListener('click', function(event) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+});
