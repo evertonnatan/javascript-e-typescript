84af9d3aa5cb025bba0c7260ce84bbb19009a7b4
evertonnatan <evertonnatandl@outlook.com>
2021-07-25 09:52:12 -0300

    Aprendendo sobre hoisting, tratamento de funções e first-class objects

--- 04 - funcoes avancado/hoisting.js
+++ 04 - funcoes avancado/hoisting.js
@@ -18,3 +18,31 @@ falaOi();
 /**
  * Funções podem ser tratadas como dados (First-class Objects)
  */
+
+// EXEMPLO:
+const souUmDado = function() {
+    console.log("Sou um dado.");
+};
+
+function executaFuncao(funcao) {
+    console.log("Vou executar sua função abaixo");
+    funcao();
+};
+executaFuncao(souUmDado);
+
+
+// FUNÇÕES DENTRO DE OBJETOS (como métodos e notação de ponto)
+const obj = {
+    falar: function() {
+        console.log("Estou falando...");
+    }
+};
+obj.falar();
+
+// FUNÇÕES DENTRO DE OBJETOS (como métodos e sem a notação de ponto)
+const obj2 = {
+    comer() {
+        console.log("Estou comendo...");
+    }
+};
+obj2.comer();
