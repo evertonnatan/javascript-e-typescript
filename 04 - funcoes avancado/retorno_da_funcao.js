34fc8011b615128202defd2285f8fa9732f2fb18
evertonnatan <evertonnatandl@outlook.com>
2021-08-04 09:28:55 -0300

    Entendo o retorno (return) da função

---
+++ 04 - funcoes avancado/retorno_da_funcao.js
@@ -0,0 +1,15 @@
+// Entendendo o escopo léxico das funções (closures)
+
+function criaMultiplicador(multiplicador) {
+    return function(n) {
+        return n * multiplicador;
+    };
+};
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(40));
+console.log(triplica(60));
+console.log(quadriplica(90));
