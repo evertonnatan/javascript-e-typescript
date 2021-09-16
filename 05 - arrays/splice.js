158bf023107526dfb7c682d8292f0d57f6a1f284
evertonnatan <evertonnatandl@outlook.com>
2021-09-15 21:13:14 -0300

    Aula do método splice

---
+++ 05 - arrays/splice.js
@@ -0,0 +1,25 @@
+//
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+
+// nomes.splice(indice, delete, elem1, elem2, elem3);
+
+const removidos = nomes.splice(3 ,2);
+console.log(nomes, removidos);
+
+/**
+ * (3) ['Maria', 'João', 'Eduardo']
+0: "Maria"
+1: "João"
+2: "Eduardo"
+length: 3
+[[Prototype]]: Array(0)
+ 
+(2) ['Gabriel', 'Julia']
+0: "Gabriel"
+1: "Julia"
+length: 2
+
+ */
+
+// Número máximo que o JavaScript retorna:
+console.log(Number.MAX_VALUE) // 1.7976931348623157e+308
