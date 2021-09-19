647206394b956f399a465a3bb4137ceba0aabc35
evertonnatan <evertonnatandl@outlook.com>
2021-09-19 20:06:39 -0300

    Filter - exemplo 02 da aula

--- 05 - arrays/filter.js
+++ 05 - arrays/filter.js
@@ -13,3 +13,18 @@ function callbackFilter(valor, indice) {
 
 const numerosFiltrados = numeros.filter(callbackFilter);
 console.log(numerosFiltrados)
+
+// EXEMPLO 02:
+
+// Nosso exemplo deverá retornar as pessoas que têm o nome com 5 letras ou mais
+// Nosso exemplo deverá retornar as pessoas que têm mais de 50 anos
+// Nosso exemplo deverá retornar as pessoas cujos nomes terminam com "a"
+
+const pessoas = [
+    { nome: "Luiz", idade: 62 },
+    { nome: "Maria", idade: 23 },
+    { nome: "Eduardo", idade: 55 },
+    { nome: "Letícia", idade: 19 },
+    { nome: "Rosana", idade: 32 },
+    { nome: "Wallace", idade: 47 },
+]
