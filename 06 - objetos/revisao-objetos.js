049ac2b851aaa4593085336b24d9888072217e50
evertonnatan <evertonnatandl@outlook.com>
2021-09-24 09:35:54 -0300

    Revisando objetos em JS

---
+++ 06 - objetos/revisao-objetos.js
@@ -0,0 +1,26 @@
+// NOTAÇÕES DE PONTO OU DE COLCHETES
+/**https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Property_Accessors
+ * 
+ * Os assessores de propriedade fornecem acesso as propriedades de um objeto usando a notação de ponto ou a notação 
+ * de colchetes.
+
+Sintaxe
+object.property
+object["property"]
+ */
+
+// EXEMPLO PRÁTICO
+const pessoa1 = new Object();
+pessoa1.nome = "Everton";
+pessoa1.sobrenome = "Natan";
+pessoa1.idade = 26;
+pessoa1.falarNome = function() {
+    return (`${this.nome} está falando seu nome.`);
+};
+
+pessoa1.getDataNascimento = function() {
+    const dataAtual = new Date();
+    return dataAtual.getFullYear() - this.idade;
+}
+const pessoa2 = (`${pessoa1.nome} nasceu no ano de`)
+console.log(pessoa2, + pessoa1.getDataNascimento());
