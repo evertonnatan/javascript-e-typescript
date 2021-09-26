bcf5c4251fa042460022b634f582c293dd675252
evertonnatan <evertonnatandl@outlook.com>
2021-09-26 09:16:56 -0300

    Object.freeze

--- 06 - objetos/metodosUteis.js
+++ 06 - objetos/metodosUteis.js
@@ -23,9 +23,53 @@ my_obj.foo = 1;
 console.log(Object.keys(my_obj)); // console: ['foo']
 
 
-
-
 // Object.freeze
+
+/**O método Object.freeze() congela um objeto: isto é, impede que novas propriedades sejam adicionadas a ele; 
+ * impede que as propriedades existentes sejam removidas; e impede que propriedades existentes, 
+ * ou sua inumerabilidade, configurabilidade, ou capacidade de escrita sejam alteradas. 
+ * Em essência o objeto é efetivamente imutável. O método retorna o objeto congelado. */
+
+// Exemplos
+
+var obj = {
+    prop: function() {},
+    foo1: 'bar'
+  };
+  
+  // Novas propriedades podem ser adicionadas, propriedades existentes podem ser alteradas ou removidas
+  obj.foo1 = 'baz';
+  obj.lumpy = 'woof';
+  delete obj.prop;
+  
+  // Tanto o objeto que está sendo passado, bem como o objeto retornado será congelado.
+  // É desnecessário salvar o objeto retornado para congelar o objeto original.
+  var o = Object.freeze(obj);
+  
+  o === obj; // true
+  Object.isFrozen(obj); // === true
+  
+  // De agora em diante qualquer alteração irá falhar
+  obj.foo1 = 'quux'; // silenciosamente não faz nada.
+  obj.quaxxor = 'the friendly duck'; // silenciosamente não adiciona a propriedade
+  
+  // ...e em modo strict tais tentativas irão lançar TypeErrors
+  function fail(){
+    'use strict';
+    obj.foo1 = 'sparky'; // throws um TypeError
+    delete obj.quaxxor; // throws um TypeError
+    obj.sparky = 'arf'; // throws um TypeError
+  }
+  
+  fail();
+  
+  // As tentativas de alteração através de Object.defineProperty também irão lançar
+  Object.defineProperty(obj, 'ohai', { value: 17 }); // throws um TypeError
+  Object.defineProperty(obj, 'foo', { value: 'eit' }); // throws um TypeError
+  
+
+
+
 // Object.defineProperties
 // Object.defineProperty
 // Object.values
