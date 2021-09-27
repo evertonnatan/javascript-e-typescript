1a70d9827d87aa09f9e8e9a9c6cfd8b12f6c2b19
evertonnatan <evertonnatandl@outlook.com>
2021-09-27 09:14:32 -0300

    Starting to delve into Prototypes, in JavaScript objects.

---
+++ 06 - objetos/Prototypes.js
@@ -0,0 +1,8 @@
+/**Protótipos são o mecanismo pelo qual objetos JavaScript herdam recursos uns dos outros. 
+ * Toda função de construtor possui uma propriedade prototype cujo valor é um objeto que contém uma propriedade constructor. 
+ * Esta propriedade construtora aponta para a função construtora original
+ * 
+ * 
+ * 
+ * 
+ */
