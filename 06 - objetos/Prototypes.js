ec5e688ae1479bcf230a26f4638ccbe5c09f6f9f
evertonnatan <evertonnatandl@outlook.com>
2021-09-28 08:23:45 -0300

    Finalizando aula sobre manipulaçao de Prototypes

--- 06 - objetos/Prototypes.js
+++ 06 - objetos/Prototypes.js
@@ -6,3 +6,49 @@
  * 
  * 
  */
+
+// new Object => Object.prototype
+// const objA = {
+//     chaveA: 'A'
+//     // __proto__: Object.prototype
+// };
+
+// const objB = {
+//     chaveB: 'B'
+//     // __proto__: objA
+// };
+
+// const objC = new Object();
+// objC.chaveC = 'C';
+
+// Object.setPrototypeOf(objB, objA);
+// Object.setPrototypeOf(objC, objB);
+// console.log(objC.chaveA);
+
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.desconto = function(percentual) {
+    this.preco = this.preco - (this.preco * (percentual / 100));
+};
+
+Produto.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+}
+
+const p1 = new Produto('camiseta', 50);
+
+// Literal:
+const p2 = {
+    nome: 'Caneca',
+    preco: 30
+};
+Object.setPrototypeOf(p2, Produto.prototype);
+p2.aumento(10);
+
+
+
+console.log(p1);
+console.log(p2);
