ed632aedb2b6e20c56d706392ea81d5b0e5f6799
evertonnatan <evertonnatandl@outlook.com>
2021-10-01 21:58:13 -0300

    Assistindo a resolução do professor

--- 06 - objetos/validandoCPF.js
+++ 06 - objetos/validandoCPF.js
@@ -19,29 +19,69 @@ Observação Importante: Se o resto da divisão for igual a 10, nós o considera
  * 
  */
 
-let cpf = '705.484.450-52';
-let cfpClean = cpf.replace(/\D+/g, '');
-cpfArray = Array.from(cfpClean);
+// RESOLUÇÃO DO PROFESSOR:
 
-let dig1 = cpfArray[0] * 10;
-let dig2 = cpfArray[1] * 9;
-let dig3 = cpfArray[2] * 8;
-let dig4 = cpfArray[3] * 7;
-let dig5 = cpfArray[4] * 6;
-let dig6 = cpfArray[5] * 5;
-let dig7 = cpfArray[6] * 4;
-let dig8 = cpfArray[7] * 3;
-let dig9 = cpfArray[8] * 2;
+// 705.484.450-52 070.987.720-03
+/*
+7x  0x 5x 4x 8x 4x 4x 5x 0x
+10  9  8  7  6  5  4  3  2
+70  0  40 28 48 20 16 15 0 = 237
 
-const soma = dig1 + dig1 + dig3 + dig4 + dig5 + dig6 + dig7 + dig8 + dig9;
+11 - (237 % 11) = 5 (Primeiro dígito)
+Se o número digito for maior que 9, consideramos 0.
 
-const validacao1 = soma * 10 / 11
-const validacao2 = validacao1 % 11
+7x  0x 5x 4x 8x 4x 4x 5x 0x 5x
+11 10  9  8  7  6  5  4  3  2
+77  0  45 32 56 24 20 20 0  10 = 284
 
-
-console.log(soma)
-console.log(validacao2)
-console.log(cpfArray[2]);
-
-
-console.log(cpfArray.reduce((ac, val) => ac + Number(val), 0));
+11 - (284 % 11) = 2 (Primeiro dígito)
+Se o número digito for maior que 9, consideramos 0.
+*/
+function ValidaCPF(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+      enumerable: true,
+      get: function() {
+        return cpfEnviado.replace(/\D+/g, '');
+      }
+    });
+  }
+  
+  ValidaCPF.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
+  
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+  
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
+  };
+  
+  ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+  
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+      ac += (regressivo * Number(val));
+      regressivo--;
+      return ac;
+    }, 0);
+  
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
+  };
+  
+  ValidaCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+  };
+  
+  const cpf = new ValidaCPF('070.987.720-03');
+  
+  if(cpf.valida()) {
+    console.log('Cpf válido');
+  } else {
+    console.log('Cpf inválido');
+  }
